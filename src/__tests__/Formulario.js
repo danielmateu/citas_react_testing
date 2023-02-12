731e37e4fbb9ca2e37eeaa3e9358171b653da0b6
Daniel Mateu <danielmateu86@gmail.com>
2023-02-13 00:54:13 +0100

    Test para el rellenado de los datos del form

--- src/__tests__/Formulario.js
+++ src/__tests__/Formulario.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { fireEvent, render, screen, cleanup } from "@testing-library/react"
 import Formulario from "../components/Formulario"
 import '@testing-library/jest-dom/extend-expect'
+import userEvent from '@testing-library/user-event';
 
 
 const crearCita = jest.fn();
@@ -48,6 +49,67 @@ test('<Formulario> Validación del Formulario', () => {
     expect(alerta.tagName).toBe('P')
     expect(alerta.tagName).not.toBe('BUTTON')
 })
+
+test('<Formulario> Validación del formulario', () => {
+    render(<Formulario crearCita={crearCita}/>)
+
+    //Llenar el formulario
+
+    userEvent.type(screen.getByTestId('mascota'), 'Nuk')
+    userEvent.type(screen.getByTestId('propietario'), 'Dani')
+    userEvent.type(screen.getByTestId('fecha'), '2021-09-10')
+    userEvent.type(screen.getByTestId('hora'), '10:30')
+    userEvent.type(screen.getByTestId('sintomas'), 'El més guapo')
+
+    fireEvent.change(screen.getByTestId('mascota'), {
+        target: {
+            value: 'Nuk'
+        }
+    })
+
+    // fireEvent.change(screen.getByTestId('propietario'), {
+    //     target: {
+    //         value: 'Dani'
+    //     }
+    // })
+
+
+    // fireEvent.change(screen.getByTestId('fecha'), {
+    //     target: {
+    //         value: '2021-09-10'
+    //     }
+    // })
+
+
+    // fireEvent.change(screen.getByTestId('hora'), {
+    //     target: {
+    //         value: '10:30'
+    //     }
+    // })
+
+
+    // fireEvent.change(screen.getByTestId('sintomas'), {
+    //     target: {
+    //         value: 'El més guapo'
+    //     }
+    // })
+
+    
+    //Hacer submit
+    // fireEvent.click(screen.getByTestId('btn-submit'))
+    const btnSubmit = screen.getByTestId('btn-submit')
+    userEvent.click(btnSubmit);
+
+    //Revisar que el error no exista
+    const alerta = screen.queryByTestId('alerta')
+    expect(alerta).not.toBeInTheDocument()
+
+    //Revisar que la función se haya llamado
+    expect(crearCita).toHaveBeenCalled()
+    expect(crearCita).toHaveBeenCalledTimes(1)
+})
+
+
 
 
 
